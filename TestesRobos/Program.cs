afe5f3aa297e0e4c54c7527436035570490848f0
Osnildo <juniorosnildo777@gmail.com>
2025-05-13 18:16:53 -0300

    aaaaaa

---
+++ TestesRobos/Program.cs
@@ -0,0 +1,21 @@
+﻿using ControladorDeRobos.Services;
+using ControladorDeRobos.Services.Buscas;
+
+var mapa = new MapaService();
+mapa.GerarMapa();
+var robos = new RoboSevice();
+robos.PosicionarRobos();
+
+var algoritimo = BuscaFactory.GetAlgoritmo("largura");
+var (melhorRobo, melhorCaminho) = 
+    BuscaService.EncontrarRoboMaisProximo(algoritimo, 10, 0);
+
+var rota = RotaService.GerarRotaCompleta(melhorCaminho, 10, 0);
+
+foreach (var aa in rota)
+{
+    Console.WriteLine(aa.x + " " + aa.y);
+}
+
+Console.WriteLine();
+Console.WriteLine(melhorRobo);
